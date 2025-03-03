43234cd56bbae22c54deecfe3eac438ac7e3405e
MarcelMikucewicz <marcelmikucewicz@gmail.com>
2025-03-03 12:22:56 +0100

    lekcja1

---
+++ script.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const mysql = require("mysql")
+ 
+const app = express()
+var conn = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "projekt_porownywanie"
+})
+ 
+ 
+conn.connect((err)=>{
+    if(err){
+        console.log("nie połączono z bazą")
+    } else{
+        console.log("połaczono z bazą")
+    }
+})
+ 
